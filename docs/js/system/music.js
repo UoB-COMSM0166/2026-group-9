b91af91755789c9f6fd8aa9bac32526a9226c61c
Jack8ot <offjjx@gmail.com>
2026-04-17 16:53:16 +0100

    musicfix

--- docs/js/system/music.js
+++ docs/js/system/music.js
@@ -4,11 +4,16 @@ function playStageBGM(newBGM){
     if(currentPlayingBGM == newBGM) return;
 
     if(currentPlayingBGM && currentPlayingBGM.isPlaying()){
+        console.log("Stopping current BGM");
         currentPlayingBGM.stop();
 }
     currentPlayingBGM = newBGM;
     if(currentPlayingBGM){
+        console.log("Playing new BGM");
         currentPlayingBGM.loop();
-        currentPlayingBGM.setVolume(0.5);
+        currentPlayingBGM.setVolume(1.0);
+    }
+    else{
+        console.log("No BGM to play");
     }
 }
