a472cc6eecaafcf688f779c23bc28db91f1484d0
xusinan-sys <2425635212@qq.com>
2026-04-19 00:25:00 +0100

    j

--- docs/js/system/ui.js
+++ docs/js/system/ui.js
@@ -36,11 +36,11 @@ function drawGameContent() {
             if (e.flashTimer % 6 === 0) {
                 tint(255, 150);
             } else {
-                image(enemyImages[e.imgKey], e.x, e.y, e.size * 2, e.size * 2);
+                image(enemyImages[e.imgKey], e.x, e.y, e.size * 2.5, e.size * 1.8);
             }
             e.flashTimer--;
         } else {
-            image(enemyImages[e.imgKey], e.x, e.y, e.size * 2, e.size * 2);
+            image(enemyImages[e.imgKey], e.x, e.y, e.size * 2.5, e.size * 1.8);
         }
 
         pop();
