15391210ba3e980dbedce231b2ce66de5661110d
xusinan-sys <2425635212@qq.com>
2026-04-18 22:45:00 +0100

    j

--- docs/js/entity/player.js
+++ docs/js/entity/player.js
@@ -25,7 +25,7 @@ function drawPlayer() {
         currentPlayerImg,
         player.x - player.size ,
         player.y - player.size / 2,
-        player.size,
+        player.size*2,
         player.size
     );
 }
