642a85cc5e3aecd12b9c269e8a118d69460b9efe
Jack8ot <offjjx@gmail.com>
2026-04-18 20:56:00 +0100

    Bugfix

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -11,12 +11,12 @@ function preload() {
     endingBGM = loadSound('asset/BGM/Ending.mp3'); 
     enemyDeathSound = loadSound('asset/audio/enemy-death.wav');
 
-    enemyImages = {};
-    enemyImages.enemy1 = loadImage('asset/enemy/enemy1.png');
-    enemyImages.enemy2 = loadImage('asset/enemy/enemy2.png');
-    enemyImages.enemy3 = loadImage('asset/enemy/enemy3.png');
-    enemyImages.enemy4 = loadImage('asset/enemy/enemy4.png');
-    enemyImages.enemy5 = loadImage('asset/enemy/enemy5.png');
+enemyImages = {}; 
+enemyImages.enemy1 = loadImage('asset/enemy/enemy1.png');
+enemyImages.enemy2 = loadImage('asset/enemy/enemy2.png');
+enemyImages.enemy3 = loadImage('asset/enemy/enemy3.png');
+enemyImages.enemy4 = loadImage('asset/enemy/enemy4.png');
+enemyImages.enemy5 = loadImage('asset/enemy/enemy5.png');
  //new add
 }
 
