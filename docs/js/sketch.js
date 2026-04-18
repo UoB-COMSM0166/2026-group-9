c5872c8e3864ac4dde26afa7d61c8f981b4617e3
xusinan-sys <2425635212@qq.com>
2026-04-18 23:27:51 +0100

    j

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -1,62 +1,12 @@
-console.log("🚀 js/sketch.js 文件已加载！");
+let bgm01, bgm02, bgm03, bgmmenu;
 
+function preloadAssets() {
+    bgImg1 = loadImage('asset/image/background/01.png');
+    bgImg2 = loadImage('asset/image/background/02.png');
+    bgImg3 = loadImage('asset/image/background/03.png');
 
-
-function preload() {
-    preloadAssets();
-    playerImgs={};//new add for image
-    playerImgs[1]= loadImage('asset/role/设计拟人化圆形并保存.png');
-    playerImgs[2]= loadImage('asset/role/第二关player.png');
-    playerImgs[3]= loadImage('asset/role/第三关player.png');
-
-    //new add for image
-    for(let i = 0; i < 10; i++){
-        comicImages[i] = loadImage(`asset/image/Comic/C${i}.png`);
-    }
-    endingBGM = loadSound('asset/BGM/Ending.mp3'); // 請確保路徑正確
-    enemyDeathSound = loadSound('asset/audio/enemy-death.wav');
-
-    enemyImages = {};
-  // 加载5种敌人的图片
-    enemyImages.enemy1 = loadImage('asset/enemy/enemy1.png');
-    enemyImages.enemy2 = loadImage('asset/enemy/enemy2.png');
-    enemyImages.enemy3 = loadImage('asset/enemy/enemy3.png');
-    enemyImages.enemy4 = loadImage('asset/enemy/enemy4.png');
-    enemyImages.enemy5 = loadImage('asset/enemy/enemy5.png');
- //new add
-    console.log("✅ preload 执行完毕！");
-}
-
-function setup() {
-    console.log("🎮 setup 开始执行");
-    createCanvas(960, 540);
-    resetPlayer();
-
-    currentPlayerImg=playerImgs[1];//new add for image
-}
-
-function draw() {
-    background(40);
-
-    sceneSwitch();
-}
-
-function updateGame() {
-
-
-    handlePlayerMovement();
-    handleShooting();
-
-
-    spawnEnemies();
-
-    updateObjects();
-    updateTimer();
-
-    checkProgress();
-    checkGameOver();
-}
-
-function updatePlayerSprite() {
-  currentPlayerImg = playerImgs[currentLevel] || playerImgs[1];
+    bgm01 = loadSound('./asset/BGM/Level01.mp3');
+    bgm02 = loadSound('./asset/BGM/Level02.mp3');
+    bgm03 = loadSound('./asset/BGM/Level03.mp3');
+    bgmmenu = loadSound('./asset/BGM/StartMenu.mp3');
 }
