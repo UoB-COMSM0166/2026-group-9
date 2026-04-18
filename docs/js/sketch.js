43e667fe1383d3990438189538709774498d3baf
xusinan-sys <2425635212@qq.com>
2026-04-18 23:36:40 +0100

    k

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -1,12 +1,50 @@
-let bgm01, bgm02, bgm03, bgmmenu;
+let playerImg; // new add for image
+let enemyImages={};
 
-function preloadAssets() {
-    bgImg1 = loadImage('asset/image/background/01.png');
-    bgImg2 = loadImage('asset/image/background/02.png');
-    bgImg3 = loadImage('asset/image/background/03.png');
+function preload() {
+    preloadAssets();
+    playerImg = loadImage('asset/role/主角01.png');
+    //new add for image
+    for(let i = 0; i < 10; i++){
+        comicImages[i] = loadImage(`asset/image/Comic/C${i}.png`);
+    }
+    endingBGM = loadSound('asset/BGM/Ending.mp3'); 
+    enemyDeathSound = loadSound('asset/audio/enemy-death.wav');
 
-    bgm01 = loadSound('./asset/BGM/Level01.mp3');
-    bgm02 = loadSound('./asset/BGM/Level02.mp3');
-    bgm03 = loadSound('./asset/BGM/Level03.mp3');
-    bgmmenu = loadSound('./asset/BGM/StartMenu.mp3');
+    enemyImages = {};
+    enemyImages.enemy1 = loadImage('asset/enemy/基礎敵人.png');
+    enemyImages.enemy2 = loadImage('asset/enemy/快跑敵人.png');
+    enemyImages.enemy3 = loadImage('asset/enemy/坦克敵人.png');
+    enemyImages.enemy4 = loadImage('asset/enemy/分裂敵人.png');
+    enemyImages.enemy5 = loadImage('asset/enemy/游移敵人.png');
+ //new add
+}
+
+function setup() {
+    createCanvas(960, 540);
+    resetPlayer();
+}
+
+function draw() {
+    background(40);
+
+    sceneSwitch();
+
+    updateGameState();
+}
+
+function updateGame() {
+
+
+    handlePlayerMovement();
+    handleShooting();
+
+
+    spawnEnemies();
+
+    updateObjects();
+    updateTimer();
+
+    checkProgress();
+    checkGameOver();
 }
