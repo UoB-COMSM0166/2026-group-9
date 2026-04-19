15d8b0879e92b12a9f6bd7129d0adad0950ea269
Jack8ot <offjjx@gmail.com>
2026-04-19 18:16:55 +0100

    bugfix

--- docs/js/system/glitch.js
+++ docs/js/system/glitch.js
@@ -18,14 +18,25 @@ const GlitchManager = {
 
     // 這個 draw 已經做得很棒了，會自己處理抖動和透明度
     draw: function(img) {
-        if (this.alpha <= 0) return; 
-        push();
-        tint(255, this.alpha);
-        let jitterX = random(-3, 3);
-        let jitterY = random(-3, 3);
-        imageMode(CENTER); // 確保對齊
-        image(img, width/2 + jitterX, height/2 + jitterY, width * 0.8, (img.height/img.width) * (width * 0.8));
-        noTint();
-        pop();
+    // 1. 偵錯：先確認圖片有沒有載入成功
+    if (!img) {
+        console.error("DEBUG: 圖片 img 是空的 (undefined 或 null)！");
+        return;
     }
-};
+    
+    // 2. 偵錯：確認 alpha 值到底是多少
+    console.log("DEBUG: GlitchManager alpha 值:", this.alpha);
+    
+    // 如果 alpha 小於等於 0，那就真的畫不出來
+    if (this.alpha <= 0) return; 
+    
+    push();
+    tint(255, this.alpha);
+    let jitterX = random(-3, 3);
+    let jitterY = random(-3, 3);
+    imageMode(CENTER);
+    image(img, width/2 + jitterX, height/2 + jitterY, width * 0.8, (img.height/img.width) * (width * 0.8));
+    noTint();
+    pop();
+}
+}
